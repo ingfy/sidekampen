ea3cb98e69da53e803509c92ae98ac1843b9bf83
Yngve Svalestuen <yngve@outlook.com>
2023-09-26 20:44:32 +0200

    Add publish script

---
+++ script/ignore.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+// Modifies .gitignore to remove bundle.js
+import { readFile, writeFile } from "fs";
+import { resolve } from "path";
+
+var cwd = process.cwd();
+var ignores = resolve(cwd, ".gitignore");
+
+readFile(ignores, "utf8", function (err, data) {
+  if (err) {
+    console.error("No .gitignore found to modify, skipping.");
+    process.exit(0);
+  }
+  data = data.replace(/^dist\/[\n\r]?/gm, "");
+  writeFile(ignores, data, function (err) {
+    if (err) {
+      console.error("Could not modify .gitignore");
+      throw err;
+    }
+  });
+});
