0255f7c39e1386075654023af359408a4f909016
Yngve Svalestuen <yngve@outlook.com>
2023-11-19 13:46:43 +0100

    add base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ingfy.github.io/sidekampen"
+    site: "https://ingfy.github.io",
+    base: "/sidekampen"
 });
